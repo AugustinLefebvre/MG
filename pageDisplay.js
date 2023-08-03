a67db2ac95d04580d2940cfe7c7d40d7746dfc51
Augustin Lefebvre <17476911+Gugusteh@users.noreply.github.com>
2023-08-03 23:56:53 +0200

    fix chrome reviews, feedback based improvements

--- pageDisplay.js
+++ pageDisplay.js
@@ -6,11 +6,11 @@ function decryptContact(code, contact = "mailto:") {
 $(document).ready(function () {
     // contact info parser
     $('#mg-contact-info').append(atob('bWcubGVmZWJ2cmVAdmlhLWFwLmNvbQ'));
-    $('#mg-contact-info-wa').append(atob('KzQ0IDc5MjIgOTIzMDkw'));
+    $('#mg-contact-info-wa').append(atob('KzQ0IDcgOTIyIDkyMyAwOTA'));
     $('#mg-contact-info-tel').append(atob('KzMzIDYgMDIgMDggNTEgMzI'));
     $('#mg-footer-info').append(atob('bWcubGVmZWJ2cmVAdmlhLWFwLmNvbQ'));
-    $('#mg-footer-info-wa1').append(atob('KzQ0IDc5MjIgOTIzMDkw'));
-    $('#mg-footer-info-wa2').append(atob('KzQ0IDc5MjIgOTIzMDkw'));
+    $('#mg-footer-info-wa1').append(atob('KzQ0IDcgOTIyIDkyMyAwOTA'));
+    $('#mg-footer-info-wa2').append(atob('KzQ0IDcgOTIyIDkyMyAwOTA'));
     $('#mg-footer-info-tel').append(atob('KzMzIDYgMDIgMDggNTEgMzI'));
 
 
@@ -23,11 +23,11 @@ $(document).ready(function () {
                 .css('max-height', 'unset')
                 .css('overflow', 'visible')
                 .css('-webkit-line-clamp', '20');
-            $elem.parents('.review-text').css('height', 'unset');
+            $elem.parents('.review-text').css('height', $elem.parents('.review-text')[0].scrollHeight + 'px');
 
         } else if(($elem[0].scrollHeight == $elem.height()) || ($elem.parents('.review-text').height() !== 200)) {
             $elem
-                .css('max-height', '200px')
+                .css('max-height', '168px')
                 .css('overflow', 'hidden')
                 .css('-webkit-line-clamp', '7');
             $elem.parents('.review-text').css('height', '200px');
