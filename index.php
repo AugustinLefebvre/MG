5559b0bf50899f249c746f4474f0735a7ab1c230
Augustin Lefebvre <17476911+Gugusteh@users.noreply.github.com>
2023-08-06 18:54:40 +0200

    translation setup

--- index.php
+++ index.php
@@ -1,62 +1,20 @@
 <?php
-// $domain = 'homepage';
-// $lang = 'fr_FR';
-// if ($_GET['locale']) {
-//     $locale = $_GET['locale'];
-//     echo $locale . '<= valeur retournée';
-// } else {
-//     $locale = 'fr_FR';
-//     echo 'default locale fr';
-// }
-// if (!setlocale(LC_ALL, $locale)) {
-//     echo 'locale not supported';
-// }
-
-// putenv("LANG=$locale");
-// putenv("LANGUAGE=$locale");
-
-// textdomain($domain);
-// bindtextdomain($domain, './translations');
-// bind_textdomain_codeset($domain, 'UTF-8');
-
-// NOUVEAU TEST 
-
-$locale = 'fr_FR';
+$locale = 'fr_FR.utf8';
 if (isset($_GET["locale"])) {
     $locale = $_GET["locale"];
 }
 $domain = 'homepage';
 
-$results = putenv("LC_ALL=$locale");
-if (!$results) {
-    exit ('putenv failed');
-}
-
-$results = setlocale(LC_ALL, $locale, 'fr_FR.utf8');
-if (!$results) {
-    exit ('setlocale failed: locale function is not available on this platform, or the given local does not exist in this environment');
-}
-
-$results = bindtextdomain($domain, "./translations");
-echo 'new text domain is set: ' . $results. "\n";
-
-$results = textdomain($domain);
-echo 'current message domain is set: ' . $results. "\n";
-
-$results = gettext("translation test");
-if ($results === "translation test") {
-    echo "Original English was returned. Something wrong\n";
-}
-echo $results . "\n";
+putenv("LC_ALL=$locale");
+setlocale(LC_ALL, $locale, 'fr_FR', '');
+bindtextdomain($domain, "./translations");
+textdomain($domain);
 
 ?>
 <!doctype html>
 <html lang="<?=$locale?>">
     <?php include('./head.php');?>
     <body>
-        <?= _('translation test');?>
-        <?= gettext('second translation test');?>
-
         <?php
             include('./navbar.php');
             include('./homepage.php');
