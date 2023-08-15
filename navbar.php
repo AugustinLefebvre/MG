d83d1a9f3d63719e0cc79b4235bdd3ad13d201e0
Augustin Lefebvre <17476911+Gugusteh@users.noreply.github.com>
2023-08-15 19:03:16 +0200

    fix button header button issue

--- navbar.php
+++ navbar.php
@@ -10,10 +10,10 @@
     </button>
     <div class="collapse navbar-collapse nav-list-container" id="nav-list">
         <ul class="navbar-nav">
-            <li><a class="nav-link" href="#certif"><span data-bs-toggle="collapse" data-bs-target="#nav-list"><?= _('who-am-i');?></span></a></li>
-            <li><a class="nav-link" href="#section-CGP-1"><span data-bs-toggle="collapse" data-bs-target="#nav-list"><?= _('Wealth Management');?></span></a></li>
-            <li><a class="nav-link" href="#section-courtage-1"><span data-bs-toggle="collapse" data-bs-target="#nav-list"><?= _('Brokerage');?></span></a></li>
-            <li><a class="nav-link" href="#section-coaching-1"><span data-bs-toggle="collapse" data-bs-target="#nav-list"><?= _('Financial Coaching');?></span></a></li>
+            <li><a class="nav-link" href="#certif"><div data-bs-toggle="collapse" data-bs-target="#nav-list"><?= _('who-am-i');?></div></a></li>
+            <li><a class="nav-link" href="#section-CGP-1"><div data-bs-toggle="collapse" data-bs-target="#nav-list"><?= _('Wealth Management');?></div></a></li>
+            <li><a class="nav-link" href="#section-courtage-1"><div data-bs-toggle="collapse" data-bs-target="#nav-list"><?= _('Brokerage');?></div></a></li>
+            <li><a class="nav-link" href="#section-coaching-1"><div data-bs-toggle="collapse" data-bs-target="#nav-list"><?= _('Financial Coaching');?></div></a></li>
         </ul>
     </div>
 </nav>
